b7bf246f7b3740bc67d66b7574e609cc03659306
nathanardentheo <150811638+nathanardentheo@users.noreply.github.com>
2023-12-19 20:24:56 +0700

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -107,5 +107,5 @@ async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
-bot.run('MTE3OTAzNzkyODE3Mzg2MjkxMg.GxIO4w.aPYE2n8XpZ-3m7PINcYgke1iQot0uhqq0SjYEU')
+bot.run('')
     
